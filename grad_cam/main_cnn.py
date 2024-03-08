b1a5e00540bcc5045b707140b6d49d456e4f80ab
YuYuYu <141175829+YubiaoYue@users.noreply.github.com>
2024-03-08 12:12:10 +0800

    Add files via upload

---
+++ grad_cam/main_cnn.py
@@ -0,0 +1,57 @@
+import os
+import numpy as np
+import torch
+from PIL import Image
+import matplotlib.pyplot as plt
+from torchvision import models
+from torchvision import transforms
+from utils import GradCAM, show_cam_on_image, center_crop_img
+
+
+def main():
+    model = models.mobilenet_v3_large(pretrained=True)
+    target_layers = [model.features[-1]]
+
+    # model = models.vgg16(pretrained=True)
+    # target_layers = [model.features]
+
+    # model = models.resnet34(pretrained=True)
+    # target_layers = [model.layer4]
+
+    # model = models.regnet_y_800mf(pretrained=True)
+    # target_layers = [model.trunk_output]
+
+    # model = models.efficientnet_b0(pretrained=True)
+    # target_layers = [model.features]
+
+    data_transform = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+    # load image
+    img_path = "both.png"
+    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    img = Image.open(img_path).convert('RGB')
+    img = np.array(img, dtype=np.uint8)
+    # img = center_crop_img(img, 224)
+
+    # [C, H, W]
+    img_tensor = data_transform(img)
+    # expand batch dimension
+    # [C, H, W] -> [N, C, H, W]
+    input_tensor = torch.unsqueeze(img_tensor, dim=0)
+
+    cam = GradCAM(model=model, target_layers=target_layers, use_cuda=False)
+    target_category = 281  # tabby, tabby cat
+    # target_category = 254  # pug, pug-dog
+
+    grayscale_cam = cam(input_tensor=input_tensor, target_category=target_category)
+
+    grayscale_cam = grayscale_cam[0, :]
+    visualization = show_cam_on_image(img.astype(dtype=np.float32) / 255.,
+                                      grayscale_cam,
+                                      use_rgb=True)
+    plt.imshow(visualization)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ grad_cam/main_swin.py
@@ -0,0 +1,81 @@
+import os
+import math
+import numpy as np
+import torch
+from PIL import Image
+import matplotlib.pyplot as plt
+from torchvision import transforms
+from utils import GradCAM, show_cam_on_image, center_crop_img
+from swin_model import swin_base_patch4_window7_224
+
+
+class ResizeTransform:
+    def __init__(self, im_h: int, im_w: int):
+        self.height = self.feature_size(im_h)
+        self.width = self.feature_size(im_w)
+
+    @staticmethod
+    def feature_size(s):
+        s = math.ceil(s / 4)  # PatchEmbed
+        s = math.ceil(s / 2)  # PatchMerging1
+        s = math.ceil(s / 2)  # PatchMerging2
+        s = math.ceil(s / 2)  # PatchMerging3
+        return s
+
+    def __call__(self, x):
+        result = x.reshape(x.size(0),
+                           self.height,
+                           self.width,
+                           x.size(2))
+
+        # Bring the channels to the first dimension,
+        # like in CNNs.
+        # [batch_size, H, W, C] -> [batch, C, H, W]
+        result = result.permute(0, 3, 1, 2)
+
+        return result
+
+
+def main():
+    # 注意输入的图片必须是32的整数倍
+    # 否则由于padding的原因会出现注意力飘逸的问题
+    img_size = 224
+    assert img_size % 32 == 0
+
+    model = swin_base_patch4_window7_224()
+    # https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224.pth
+    weights_path = "./swin_base_patch4_window7_224.pth"
+    model.load_state_dict(torch.load(weights_path, map_location="cpu")["model"], strict=False)
+
+    target_layers = [model.norm]
+
+    data_transform = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+    # load image
+    img_path = "both.png"
+    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    img = Image.open(img_path).convert('RGB')
+    img = np.array(img, dtype=np.uint8)
+    img = center_crop_img(img, img_size)
+
+    # [C, H, W]
+    img_tensor = data_transform(img)
+    # expand batch dimension
+    # [C, H, W] -> [N, C, H, W]
+    input_tensor = torch.unsqueeze(img_tensor, dim=0)
+
+    cam = GradCAM(model=model, target_layers=target_layers, use_cuda=False,
+                  reshape_transform=ResizeTransform(im_h=img_size, im_w=img_size))
+    target_category = 281  # tabby, tabby cat
+    # target_category = 254  # pug, pug-dog
+
+    grayscale_cam = cam(input_tensor=input_tensor, target_category=target_category)
+
+    grayscale_cam = grayscale_cam[0, :]
+    visualization = show_cam_on_image(img / 255., grayscale_cam, use_rgb=True)
+    plt.imshow(visualization)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
